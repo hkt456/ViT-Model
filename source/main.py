810332a4e132f417e25bbdbfbc405ffa4879c2ec
Huỳnh Khắc Tâm <tamhuynhkhac@gmail.com>
2023-11-01 22:21:49 +0700

    configuring files

--- source/main.py
+++ source/main.py
@@ -18,34 +18,42 @@ test_dir = image_path / "test"
 IMG_SIZE = 224
 BATCH_SIZE = 32
 NUM_WORKERS = 0
+PATCH_SIZE = 16
+
+# Create data loaders
+def data_loaders():
+    manual_transforms = transforms.Compose([
+        transforms.Resize((IMG_SIZE, IMG_SIZE)),
+        transforms.ToTensor()
+    ])
+
+    print(f"Manually created transforms: {manual_transforms}")
+
+    train_dataloader, test_dataloader, class_names = create_dataloaders(train_dir=train_dir,
+                                                                        test_dir=test_dir,
+                                                                        transform=manual_transforms,
+                                                                        batch_size=BATCH_SIZE,)
+    print(train_dataloader)
+    print(test_dataloader)
+    print(class_names)
 
 
-manual_transforms = transforms.Compose([
-    transforms.Resize((IMG_SIZE, IMG_SIZE)),
-    transforms.ToTensor()
-])
-
-print(f"Manually created transforms: {manual_transforms}")
-
-train_dataloader, test_dataloader, class_names = create_dataloaders(train_dir=train_dir,
-                                                                    test_dir=test_dir,
-                                                                    transform=manual_transforms,
-                                                                    batch_size=BATCH_SIZE,)
-print(train_dataloader)
-print(test_dataloader)
-print(class_names)
-
-patchify = PatchEmbedding(in_channels=3,
-                          patch_size=16,
-                          embedding_dim=768)
-
-random_input_image = (1, 3, 224, 224)
-random_input_image_error = (1, 3, 250, 250) # will error because image size is incompatible with patch_size
 
 # # Get a summary of the input and outputs of PatchEmbedding (uncomment for full output)
 def get_summary_patchEmbedding():
+    random_input_image = (1, 3, 224, 224)
     summary(PatchEmbedding(),
         input_size=random_input_image, # try swapping this for "random_input_image_error"
         col_names=["input_size", "output_size", "num_params", "trainable"],
         col_width=20,
         row_settings=["var_names"])
+
+
+def __main__():
+    data_loaders()
+    get_summary_patchEmbedding() 
+
+
+
+if __name__ == "__main__":
+    __main__()
