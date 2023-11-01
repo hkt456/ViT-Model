6d597cd5592b3242422c1dec8f7a749b6e30f62f
Huỳnh Khắc Tâm <tamhuynhkhac@gmail.com>
2023-11-01 22:26:28 +0700

    added msa

---
+++ source/msa.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn
+
+class MultiheadSelfAttentionBlock(nn.Module):
+
+    def __init__(self,
+                 embedding_dim: int = 768,
+                 num_heads: int = 12,
+                 attn_dropout: float = 0):
+        super().__init__()
+
+        self.layer_norm = nn.LayerNorm(embedding_dim)
+
+        self.multihead_attn = nn.MultiheadAttention(embed_dim=embedding_dim,
+                                                    num_heads=num_heads,
+                                                    dropout=attn_dropout,
+                                                    batch_first=True)
+
+    def forward(self, x):
+        x = self.layer_norm(x)
+
+        attn_output = self.multihead_attn(query=x,
+                                          key=x,
+                                          value=x,
+                                          need_weights=False)
+        return attn_output
+
