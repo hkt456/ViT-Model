5b308a2cbfa1927227590ef71b326a2842daca8f
Huỳnh Khắc Tâm <tamhuynhkhac@gmail.com>
2023-11-01 23:21:15 +0700

    constructed ViT model

---
+++ source/ViT.py
@@ -0,0 +1,73 @@
+import torch
+from torch import nn
+
+from mlp import MLPBlock
+from msa import MultiheadSelfAttentionBlock
+from transformer_encoder import TransformerEncoderBlock
+from patch_embedding import PatchEmbedding 
+
+class ViT(nn.Module):
+    def __init__(self,
+                 img_size: int = 224,
+                 in_channels: int = 3,
+                 patch_size: int = 16,
+                 num_transformer_blocks: int = 12,
+                 embedding_dim: int = 768,
+                 mlp_size: int = 3072,
+                 num_heads: int = 12,
+                 attn_dropout: float = 0,
+                 mlp_dropout: float = 0.1,
+                 embedding_dropout: float = 0.1,
+                 num_classes: int = 1000):
+        super().__init__()
+
+        assert img_size % patch_size == 0, f"Image size must be divisible by patch size, image size: {img_size}, patch size: {patch_size}."
+
+        self.num_patches = (img_size * img_size) // (patch_size * patch_size)
+
+        self.class_embedding = nn.Parameter(data = torch.randn(1,1,embedding_dim), 
+                                            requires_grad=True)
+
+        self.position_embedding = nn.Parameter(data = torch.randn(1, self.num_patches + 1, embedding_dim),
+                                               requires_grad=True)
+
+        self.embedding_dropout  = nn.Dropout(p=embedding_dropout)
+
+        self.patch_embedding = PatchEmbedding(in_channels=in_channels,
+                                              patch_size=patch_size,
+                                              embedding_dim=embedding_dim)
+
+        self.transformer_encoder = nn.Sequential(*[TransformerEncoderBlock(embedding_dim=embedding_dim,
+                                                                        num_heads=num_heads,
+                                                                        mlp_size=mlp_size,
+                                                                        mlp_dropout=mlp_dropout]) for _ in range(num_transformer_blocks)])
+
+        self.classifier = nn.Sequential(
+            nn.LayerNorm(embedding_dim),
+            nn.Linear(in_features=embedding_dim,
+                      out_features=num_classes)
+        )
+
+    def forward(self, x):
+
+        batch_size = x.shape[0]
+
+        class_token = self.class_embedding.expand(batch_size, -1, -1)
+
+        x = self.patch_embedding(x)
+
+        x = torch.cat((class_token, x), dim=1)
+
+        x += self.position_embedding
+
+        x = self.embedding_dropout(x)
+
+        x = self.transformer_encoder(x)
+
+        x = self.classifier(x[:, 0])
+
+        return x
+
+
+
+
